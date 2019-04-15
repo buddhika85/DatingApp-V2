9f31da2bd5b7bc27e92d97e34de231a6bdcd000c
Buddhika85 <buddhika2006system@gmail.com>
2019-04-15 22:16:15 +0530

    Authentication to the API

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
@@ -11,9 +17,12 @@ namespace DatingApp.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _authRepository;
-        public AuthController(IAuthRepository authRepository)
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository authRepository, IConfiguration config)
         {
             _authRepository = authRepository;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -34,6 +43,39 @@ namespace DatingApp.API.Controllers
             };
             user = await _authRepository.Register(user, userForRegisterDto.Password);
             return StatusCode(201);   
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto loginDto)
+        {
+            var loggedInUser = await _authRepository.Login(loginDto.Username.ToLower(), loginDto.Password);
+            if (loggedInUser == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new [] 
+            {
+                new Claim(ClaimTypes.NameIdentifier, loggedInUser.Id.ToString()),
+                new Claim(ClaimTypes.Name, loginDto.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var toekenDescriptor = new SecurityTokenDescriptor 
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(toekenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -21,9 +21,30 @@ namespace DatingApp.API.Data
             return isUserExists;
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+            if (user == null)
+                return null;
+            
+            if (! VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+                return null;
+            
+            return user;
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {               
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if(computedHash[i] != passwordHash[i])
+                        return false;
+                }
+            }
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
