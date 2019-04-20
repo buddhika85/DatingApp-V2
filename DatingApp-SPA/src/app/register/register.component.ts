fb32af952e0051a8b21842c84e76621f07932e89
Buddhika85 <buddhika2006system@gmail.com>
2019-04-20 06:14:44 +0530

    Register user changes

---
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  model: any = {};
+
+  @Output() 
+  cancelRegister = new EventEmitter();
+
+  constructor(private authService : AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log(this.model);
+    }, error => {
+      console.log(error);
+    });    
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancel');
+  }
+}
